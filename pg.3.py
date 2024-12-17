d2b198b55e513ee2bb35a680b4fca8e570e16d05
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-17 20:50:53 +0530

    Create pg.3.py
    Python program to find Area of an Triangle

---
+++ pg.3.py
@@ -0,0 +1,5 @@
+#To find th earea of triangle
+b=float(input('Entere the base length of the Triangle:'))
+h=float(input('ERnter the height of the triangle:'))
+a=0.5*b*h
+print(f'The area of the triangle is:{a}')
