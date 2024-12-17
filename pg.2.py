d2bd6eeec181c9a6f30ea096a1373f8ca13772f9
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-17 20:43:24 +0530

    Create pg.2.py
    Python program to do arithmetical operation  addition and division

---
+++ pg.2.py
@@ -0,0 +1,9 @@
+#Addition
+n1=float(input('Enter num 1:'))
+n2=float(input('Enter num 2:'))
+print(f'sum:{n1}+{n2}={n1+n2}')
+#division
+n1=float(input('Enter num 1:'))
+n2=float(input('Enter num 2:'))
+if n2 == 0:print('Infinity')
+else:print(f'sum:{n1}/{n2}={n1/n2}')
