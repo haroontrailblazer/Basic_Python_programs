332fb752bc42c1c381970ef88736868b125c74a9
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-17 21:00:34 +0530

    Create pg.4.py
    Swapping using a temporary variable

---
+++ pg.4.py
@@ -0,0 +1,6 @@
+# Swapping of two variables
+a=input("Enterv the value for the first variable:")
+b=input("Enter the value for the second variable:")
+print(f'Value of a is {a} and Value of b is {b}')
+temp=a;a=b;b=temp
+print(f'After swapping the Value of a is {a} and Value of b is {b}')
