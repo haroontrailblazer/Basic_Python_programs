105ada08c77371d2ada6dab0c2019f3363a0c6fa
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-18 08:08:42 +0530

    Create pg.7.py
    Python program to convert Celsius to Fahrenheit

---
+++ pg.7.py
@@ -0,0 +1,4 @@
+#celsius to farhrenheit conversion
+c=float(input('Enter the temperature value in celsius'))
+# formula[ (celsius*9/5)+32 ]
+print(f'{c}-Celsius is equals to {(c*9/5)+32}-Fahrenheit')
