b012f83903bee23a1473fed46d8dd84c6a70324b
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-19 19:46:44 +0530

    Create pg.13.py
    Python program to check leap year

---
+++ pg.13.py
@@ -0,0 +1,4 @@
+year=int(input('Enter the year: '))
+if year%400 and year%100 == 0:print(f'{year} is a leap year')
+elif (year%400 == 0) and (year%100 != 0):print(f'{year} is a leap year')
+else:print(f'{year} is not a leap year')
